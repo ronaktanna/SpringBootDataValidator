007c57f3740b6cdc6f8ae95e77c7a9085e5f47b6
Ronak Tanna <ronak.rktanna@gmail.com>
2018-07-24 00:50:14 +0530

    Intial commit

---
+++ src/main/java/com/example/data/validator/DataValidatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.data.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataValidatorApplication.class, args);
+	}
+}
